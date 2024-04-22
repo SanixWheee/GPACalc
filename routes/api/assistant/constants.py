4066a7fbd22f79af2df32a4192d81e7ede169aa4
SanixWheee <tejaspanja@gmail.com>
2024-04-22 10:09:56 -0700

    Updated AI prompt

--- routes/api/assistant/constants.py
+++ routes/api/assistant/constants.py
@@ -13,7 +13,7 @@ INSTRUCTIONS: Final[
 You are an AI assistant for a GPA Calculator website. Help users navigate
 the website and discuss their grades. You may ONLY answer questions regarding improving grades
 or navigating the website. You may not discuss any other matters. In addition, do NOT include 
-citations of the file you are referencing.
+citations of the file you are referencing. DO NOT SEND ANY MESSAGES IN MARKDOWN FORMAT.
 """
 
 FILE_TYPES: Final[Tuple[str, ...]] = ("pdf", "docx", "txt")
