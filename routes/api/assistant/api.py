042863f4caced9a0e02592f5f5a0a711248b9d17
chawkk6404 <chawk_k@outlook.com>
2024-04-22 22:12:08 -0700

    Add logging to ai

--- routes/api/assistant/api.py
+++ routes/api/assistant/api.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
+import sys
 import glob
+import logging
 import itertools
 import os
 from typing import TYPE_CHECKING
@@ -13,6 +15,16 @@ if TYPE_CHECKING:
     from openai.types.beta import Assistant
 
 
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler(stream=sys.stderr)
+formatter = logging.Formatter('[%(asctime)s] %(levelname)s in %(name)s: %(message)s')
+handler.setFormatter(formatter)
+
+log.addHandler(handler)
+
+
 client = openai.OpenAI(api_key=os.environ["OPENAI_API_KEY"])
 
 
@@ -22,6 +34,7 @@ def init_assistant() -> Assistant:
     """
     assistant = None
 
+    log.info('Searching for existing assistant...')
     assistants = client.beta.assistants.list()
     for existing_assistant in assistants:
         if (
@@ -29,16 +42,20 @@ def init_assistant() -> Assistant:
             and existing_assistant.instructions == INSTRUCTIONS
         ):
             assistant = existing_assistant
+            log.info('Found existing assistant')
             break
 
     if assistant is None:
+        log.info('No existing assistant found, creating new assistant...')
         assistant = client.beta.assistants.create(
             name=NAME,
             instructions=INSTRUCTIONS,
             tools=[{"type": "file_search"}],
             model="gpt-3.5-turbo-0125",
         )
+        log.info('New assistant created')
 
+    log.info('Uploading training files to vector store...')
     vector_store = client.beta.vector_stores.create(name='GPA Calculator')
     file_paths = itertools.chain.from_iterable(glob.glob(f"routes/api/assistant/*.{file_type}") for file_type in FILE_TYPES)
     files = [open(path, "rb") for path in file_paths]
@@ -46,11 +63,14 @@ def init_assistant() -> Assistant:
     client.beta.vector_stores.file_batches.upload_and_poll(
         vector_store_id=vector_store.id, files=files
     )
+    log.info(f'Uploaded {len(files)} files to vector store')
 
+    log.info('Updating assistant with vector store...')
     assistant = client.beta.assistants.update(
         assistant_id=assistant.id,
         tool_resources={"file_search": {"vector_store_ids": [vector_store.id]}},
     )
 
+    log.info('Assistant initialization completed')
     return assistant
 
